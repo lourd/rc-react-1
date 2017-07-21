e6b77570071e94cfab127d42ac14c5987e01fa3e
Louis DeScioli <louis.descioli@gmail.com>
2017-07-21 14:09:59 -0400

    Adds input for changing delta value

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 class App extends Component {
   state = {
     count: 0,
+    diff: 1,
   };
 
   // This style of function declaration on a class is called a "property initializer"
@@ -12,12 +13,20 @@ class App extends Component {
   // appropriate "this".
   // See more: https://gist.github.com/lourd/8f450b609aab68d1de9b35228e784f30
   onUpClick = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ count: this.state.count + this.state.diff });
   };
 
   onDownClick = () => {
-    this.setState({ count: this.state.count - 1 });
+    this.setState({ count: this.state.count - this.state.diff });
   };
+
+  onInputChange = (event) => {
+    // event.target.value is a string, we need to make it a number
+    // console.log(typeof event.target.value)
+    // see more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+    let num = parseInt(event.target.value, 10)
+    this.setState({ diff: num })
+  }
 
   render() {
     return (
@@ -25,6 +34,7 @@ class App extends Component {
         <h1>{this.state.count}</h1>
         <button onClick={this.onUpClick}>Up</button>
         <button onClick={this.onDownClick}>Down</button>
+        <input type="number" onChange={this.onInputChange} value={this.state.diff}/>
       </div>
     );
   }
