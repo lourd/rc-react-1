2b2b61418c069be1514e2cbf0f98ba5c32ed8f5c
Louis DeScioli <louis.descioli@gmail.com>
2017-07-21 14:05:36 -0400

    Adds down button

--- src/App.js
+++ src/App.js
@@ -15,11 +15,16 @@ class App extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
+  onDownClick = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
       <div>
         <h1>{this.state.count}</h1>
         <button onClick={this.onUpClick}>Up</button>
+        <button onClick={this.onDownClick}>Down</button>
       </div>
     );
   }
